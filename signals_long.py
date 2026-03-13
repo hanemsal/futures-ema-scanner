5846ea21d140bd77f563f78b126c4542ab6efaa3
hanemsal <hakan.emsal@gmail.com>
2026-03-14 00:12:08 +0300

    Update signals_long.py

--- signals_long.py
+++ signals_long.py
@@ -3,15 +3,10 @@ def check_long_entry(df):
     prev = df.iloc[-2]
     curr = df.iloc[-1]
 
-    # EMA11 yukarı kesmiş mi
     cross_47 = prev["ema11"] <= prev["ema47"] and curr["ema11"] > curr["ema47"]
     cross_123 = prev["ema11"] <= prev["ema123"] and curr["ema11"] > curr["ema123"]
 
     if cross_47 and cross_123:
-        return True
-
-    # backup kural
-    if curr["ema11"] > curr["ema47"] and curr["ema11"] > curr["ema123"]:
         return True
 
     return False
