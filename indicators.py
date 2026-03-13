cf12a697133c24e6fd1352943390323a10aeda11
hanemsal <hakan.emsal@gmail.com>
2026-03-13 23:21:03 +0300

    Create indicators.py

---
+++ indicators.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+def calculate_ema(df, period):
+    return df['close'].ewm(span=period, adjust=False).mean()
+
+def add_ema_indicators(df):
+    df["ema11"] = calculate_ema(df, 11)
+    df["ema47"] = calculate_ema(df, 47)
+    df["ema123"] = calculate_ema(df, 123)
+    return df
