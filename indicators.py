c0b260db17b423b1f265b6dcba81587e0791a929
hanemsal <hakan.emsal@gmail.com>
2026-03-14 00:36:11 +0300

    Update indicators.py

--- indicators.py
+++ indicators.py
@@ -4,7 +4,10 @@ def calculate_ema(df, period):
     return df['close'].ewm(span=period, adjust=False).mean()
 
 def add_ema_indicators(df):
+
     df["ema11"] = calculate_ema(df, 11)
+    df["ema29"] = calculate_ema(df, 29)
     df["ema47"] = calculate_ema(df, 47)
     df["ema123"] = calculate_ema(df, 123)
+
     return df
