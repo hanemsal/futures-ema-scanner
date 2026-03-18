f2a14689d210e6ddd25633337d4892374cc71793
hanemsal <hakan.emsal@gmail.com>
2026-03-18 18:03:10 +0300

    Update backfill_risk.py

--- scripts/backfill_risk.py
+++ scripts/backfill_risk.py
@@ -1,3 +1,8 @@
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from risk_engine import BinanceRiskEngine
 from storage import SessionLocal, Signal
 
